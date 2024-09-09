33f7394a6dad54c4eaf8c2faf78f9e50ce3642f4
vpayes <vanesa_payes@outlook.com>
2024-09-08 21:45:34 -0400

    Implement and populate arrays for bills, tips, and totals

--- bill_splitter.js
+++ bill_splitter.js
@@ -16,3 +16,22 @@ let testBill = 100;
 let testTip = calculateTip(testBill);
 console.log(`For a bill of $${testBill}, the tip is $${testTip.toFixed(2)}`);
 
+// 4. Utilize Arrays 
+const bills1 = [275, 40, 430];
+const bills2 = [125. 555, 44];
+
+// First data set
+const tips1 = bills1.map(calculateTip);
+const totals1 = bills1.map((bill, index) => bill + tips1[index]);
+console.log("Data Set 1:");
+bills1.forEach((bill, index) => {
+    console.log(`The bill was $${bill}, the tip was $${tips1[index].toFixed(2)},and the total value is $${totals1[index].toFixed(2)}`);
+});
+
+//Second data set 
+const tips2 = bills2.map(calculateTip);
+const totals2 = bills2.map((bill, index) => bill + tips2[index]);
+console.log("Data Set 2:");
+bills1.forEach((bill, index) => {
+    console.log(`The bill was $${bill}, the tip was $${tips2[index].toFixed(2)},and the total value is $${totals2[index].toFixed(2)}`);
+});
