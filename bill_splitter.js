fad40a95bf9966e22af77fbdd39dd7dfe0f55a62
vpayes <vanesa_payes@outlook.com>
2024-09-08 21:35:32 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,6 +2,7 @@
 let bill = 275;
 
 // Ternary operator to determine the tip amount based on the bill size
+// 2. Output Details
 let tip = bill >= 50 && bill <= 300 ? bill * 0.20 : bill < 50 ? bill * 0.15 : bill * 0.10;
 console.log(`The bill was $${bill}, the tip was $${tip.toFixed(2)}, and the total value is $${(bill + tip).toFixed(2)}`);
 
